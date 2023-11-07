eebbb39eb1275c1815cb60bdd1fc3d460ebf0735
Kerem <kerem@oceansync.com>
2023-11-07 19:25:21 -0400

    init

---
+++ main.py
@@ -0,0 +1,18 @@
+from modules.Dataset import Dataset
+from modules.Parameter import Parameter
+from modules.PopulationList import PopulationList
+
+
+def main():
+    dataset = Dataset('dayr4.2.csv')
+    popList = PopulationList(dataset)
+
+    for generation in range(1, Parameter.generations + 1):
+        popList.generateNextGen()
+        #Display.generationReport()
+
+    #Display.overallReport()
+
+
+if __name__ == '__main__':
+    main()
